64b81b0e32fb52f15c175b85c6d560f053f16a5a
isaiah3031 <isaiah.whitfield@gmail.com>
2021-03-02 08:37:46 -0500

    Removed JS files.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'static_pages#index'
+  # root 'static_pages#index'
   
   namespace :api, defaults: { format: 'json' } do
     resources :users, only: %i[create] do
@@ -9,9 +9,9 @@ Rails.application.routes.draw do
     match "sessions", to: "sessions#destroy", via: "delete", defaults: { id: nil }
   end
 
-  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
-    !request.xhr? && request.format.html?
-  end
+  # get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+  #   !request.xhr? && request.format.html?
+  # end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
